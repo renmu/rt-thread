0ec8b5c3f1b097e81bab5aef4e405c8677747581
chenjz <chenjinzhan1985@163.com>
2019-07-29 09:19:45 +0800

    gcc build running.

--- bsp/stm32/stm32l151-ks-nano/applications/main.c
+++ bsp/stm32/stm32l151-ks-nano/applications/main.c
@@ -20,6 +20,8 @@ int main(void)
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+	  //rt_pin_write(LED0_PIN, PIN_LOW);
+	
 
     while (count++)
     {
